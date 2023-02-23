66a61e8a0d2e0b4d564d4e37765536a752ce5c14
aao056 <a_ogiolan@Yahoo.com>
2023-02-23 16:51:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import data from "./data.json"
 
 function App() {
+
+  const [items] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className='result'>
+        <h3 className='title'>Your Result</h3>
+        <div className='circle'>
+          <h5>76</h5>
+          <span>of 100</span>
+        </div>
+        <span className='rating'>Great</span>
+        <p>You scored higher than 65% of the people who have taken these tests.</p>
+      </div>
+      <div className='summary'>
+        <h3 className='title'>Summary</h3>
+        {items.map((item, index) => 
+        <div key={index}
+             className="result_item"
+             style={{
+                backgroundColor: `${item.color}15`
+             }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3 className='category' style={{
+            color: item.color
+          }}>
+            <img src={item.icon} alt={item.category}/>
+            {item.category}</h3>
+          <p>
+            <span className='score'>{item.score}</span>{" "}
+            / 100
+          </p>
+        </div>)}
+        <button className='continue'>Continue</button>
+      </div>
     </div>
   );
 }
